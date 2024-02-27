f88528661ca41011dd7ec6a03a128f96975e49f1
RomanLugovskoy <romagma29@gmail.com>
2024-02-27 18:00:54 +0300

    exercise 1

---
+++ exercise_7_6_1.js
@@ -0,0 +1,11 @@
+let word = prompt("Введите текст на проверку");
+let reverseWord = "";
+let cleanString = word.replace(/ /g, '');
+for (let i = cleanString.length - 1; i >= 0; i -=1) {
+  reverseWord = reverseWord + cleanString[i];
+}
+if (cleanString.toLowerCase() === reverseWord.toLowerCase()) {
+  console.log(`${word} является палиндромом`)
+} else {
+  console.log(`${word} не является палиндромом`)
+}
